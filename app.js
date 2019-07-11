ba3c9ddc7723a92482244f96d7d719e0d780dbdf
rodmirsantana <rodmirandasantana@gmail.com>
2019-07-11 09:58:43 -0300

    desktop version completed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const ul = document.getElementById('rooms');
+const div_cards = document.getElementById('cards');
 
 function createNode(element) {
     return document.createElement(element);
@@ -9,17 +9,25 @@ function append(parent, element) {
 }
 
 fetch('https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72')
-.then(response => response.json())
-.then(data => {
-    for(const places of Object.values(data)){
-        let li = createNode('li'),
-        texto = document.createTextNode(places["name"]),
-        // console.log(texto)
-        img = createNode('img');
-        img.src = places["photo"];
-        append(li,texto);
-        append(li,img);
-        append(ul,li);
-    }
-    
-});
+    .then(response => response.json())
+    .then(data => {
+        for(const places of Object.values(data)){
+            let div = createNode('div'),
+                p_name  = createNode('p'),
+                p_price = createNode('p'),
+                name = document.createTextNode(places["name"]),
+                price = document.createTextNode("Price: $" + places["price"] + " / night"),
+                img = createNode('img');
+            img.src = places["photo"];
+            append(p_name, name);
+            append(p_price,price);
+            append(div,img);
+            append(div,p_name);
+            append(div,p_price);
+            // div.className += "room";
+            append(div_cards,div);
+        }  
+    })
+    .catch(error => {
+        console.log("Error!");
+    });
